371dc730ef1a0033e19da6b71e6262e7333b87b0
Azzer7 <azer.balayev@gmail.com>
2018-02-21 16:56:55 +0400

    mixitup

--- js/script.js
+++ js/script.js
@@ -1,73 +1,48 @@
-// $(document).ready(function () {
-
-//     $(document).ready(function () {
-//         $(".owl-carousel").owlCarousel();
-//     });
-
-
 $(document).ready(function () {
+    
+    $(window).on('load', function () {
+        setTimeout(function () {
+            $('.loading-overlay').hide();
+        }, 1500);
+    });
+
+
     var owl = $('#owl-carousel1');
     owl.owlCarousel({
-        items:5,
-        loop:true,
-        autoplay:true,
-        autoplayTimeout:3000,
-        autoplayHoverPause:true
-        
+        items: 5,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
     });
 
     var owl2 = $('#owl-carousel2');
     owl2.owlCarousel({
-        items:1,
-        loop:true,
-        autoplay:true,
-        autoplayTimeout:3000,
-        autoplayHoverPause:true
-        
+        items: 1,
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        nav: true
     });
+
+    var mixer = mixitup('.items-container');
 
 });
 
+// $('.dropdown').hover(function () {
+//    $(this).addClass("show");
+//    $(this).find(".dropdown-menu").addClass("show");
+// },function(){
+//     $(this).removeClass("show");
+//     $(this).find(".dropdown-menu").removeClass("show");
+// });
 
-
-var portfolioIsotope = function () {
-    if ($().isotope) {
-        var $container = $('.portfolio-content');
-        $container.imagesLoaded(function () {
-            $container.isotope({
-                itemSelector: '.portfolio-item',
-                transitionDuration: '1s'
-            });
-        });
-
-        $('.portfolio-filter li').on('click', function () {
-            var selector = $(this).find("a").attr('data-filter');
-            $('.portfolio-filter li').removeClass('active');
-            $(this).addClass('active');
-            $container.isotope({
-                filter: selector
-            });
-            return false;
-        });
-    };
-}
-
-    // $('.dropdown').hover(function () {
-    //    $(this).addClass("show");
-    //    $(this).find(".dropdown-menu").addClass("show");
-    // },function(){
-    //     $(this).removeClass("show");
-    //     $(this).find(".dropdown-menu").removeClass("show");
-    // });
-
-
-
-    // $(window).scroll(function() {
-    //     console.log($(this).scrollTop());
-    //     if($(this).scrollTop()>10) {
-    //         $( "#headerA" ).addClass("scrolled2");
-    //     } else if($(this).scrollTop()<10) {
-    //         $( "#headerA" ).removeClass("scrolled2");
-    //     }
-    // });
-
+// $(window).scroll(function() {
+//     console.log($(this).scrollTop());
+//     if($(this).scrollTop()>10) {
+//         $( "#headerA" ).addClass("scrolled2");
+//     } else if($(this).scrollTop()<10) {
+//         $( "#headerA" ).removeClass("scrolled2");
+//     }
+// });
