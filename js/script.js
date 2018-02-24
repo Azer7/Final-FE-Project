5eab2e9ad1dab97dca557ab9faff9efa8825a3f3
Azzer7 <azer.balayev@gmail.com>
2018-02-25 01:09:40 +0400

    home page mega updates

--- js/script.js
+++ js/script.js
@@ -129,7 +129,17 @@ $(window).scroll(function () {
 //   }
 //   move4()
 
-
+$('.count').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 2000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
+});
 
 
 $( ".blog-ul2 a" ).hover(
